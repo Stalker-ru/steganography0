842e4b1c1996f552d21d0bb2464f6c0fb82ac41e
Stalker-ru <stalker-ru86@yandex.ru>
2014-01-15 09:56:48 +0400

    successfully hides some BMP in some WAV!

--- hiding.c
+++ hiding.c
@@ -1,10 +1,13 @@
+// hiding.c
+// steganography: hiding/finding a BMP (picture) file in a WAV (audio) file
+// Bugakov G., 2014
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <err.h>
 
 // WAVheader
-struct
-{
+struct {
 // WAV-формат начинается с RIFF-заголовка:
 
 // Содержит символы "RIFF" в ASCII кодировке
@@ -45,67 +48,92 @@ unsigned int subchunk2Size;
 }WAVheader; //далее data
 
 // BMPheader
-struct
-{
+#pragma pack(push,2)
+struct {
 // 14 B
 short signature;
 int fileSize; 
-short unused1;
-short unused2;
-int fileHeadSize;
+short reserved1;
+short reserved2;
+int offsetToData; //=54
 // 40 B	
-int headerSize;
+int thisHeaderSize; //=40
 int width; 
 int height;
 short colorDimensions;
 short density;
 int zipType;
-int massiveLength;
+int dataSize;
 int horResolution;
 int verResolution;
-int colorsQuantity;
-int shadesQuantity;
-}BMPheader; //далее data
+int colorsUsed;
+int colorsImportant;
+}BMPheader; //далее data: ColorTable (1024) + Image
+#pragma pack(pop)
 
 void info_audio () {
+printf("audio info:\n");
 // char chunkId[4]; unsigned int chunkSize; char format[4]; char subchunk1Id[4]; unsigned int subchunk1Size;
 // unsigned short audioFormat; unsigned short numChannels; unsigned int sampleRate; unsigned int byteRate;
 // unsigned short blockAlign; unsigned short bitsPerSample; char subchunk2Id[4]; unsigned int subchunk2Size;
 printf("names: %s! %s! %s! %s!\n", WAVheader.chunkId, WAVheader.format, WAVheader.subchunk1Id, WAVheader.subchunk2Id);
-printf("sizes: %u %u %u\n", WAVheader.chunkSize, WAVheader.subchunk1Size, WAVheader.subchunk2Size);
-printf("numChan; sampRate; byteRate; bps: %u %u %u %u\n", WAVheader.numChannels,
+printf("sizes: %u! %u! %u!\n", WAVheader.chunkSize, WAVheader.subchunk1Size, WAVheader.subchunk2Size);
+printf("numChan; sampRate; byteRate; bps: %u! %u! %u! %u!\n", WAVheader.numChannels,
 	WAVheader.sampleRate, WAVheader.byteRate, WAVheader.bitsPerSample);
-printf("audioFormat; blockAlign: %u %u\n", WAVheader.audioFormat, WAVheader.blockAlign);
+printf("audioFormat; blockAlign: %u! %u!\n", WAVheader.audioFormat, WAVheader.blockAlign);
+}
+
+void info_pic () {
+printf("picture info:\n");
+// 12 B: short signature; int fileSize; short unused1; short unused2; int offsetToData=54;
+// 40 B: int thisHeaderSize=40; int width; int height; short colorDimensions; short density; int zipType;
+// int massiveLength; int horResolution; int verResolution; int colorsQuantity; int shadesQuantity;
+printf("sizes: %i! %i! %i! %i!\n", BMPheader.fileSize, BMPheader.thisHeaderSize, BMPheader.offsetToData, BMPheader.dataSize);
+printf("signature: %i!\n", BMPheader.signature);
 }
 
 int main (int argc, char *argv[])
 {
-FILE *audio_file, *output_file;
-char* audioData;
+FILE *audio_file, *output_file, *picture;
+char *audioData, *picData;
 
 if (argc < 3) {
-	errx(EXIT_FAILURE,"USAGE: %s <audio_file_path> <output_file_path>", argv[0]);
+	errx(EXIT_FAILURE,"USAGE: %s <audio_file_path> <output_file_path> <pic_path>", argv[0]); //add argv[4]=mode (hide/find)
 }
 if ((audio_file = fopen (argv[1],"r")) == NULL) {
-	errx(EXIT_FAILURE,"Wrong input file name!");
+	errx(EXIT_FAILURE,"Input file cannot be opened");
 }
 if ((output_file = fopen (argv[2],"w")) == NULL) {
 	errx(EXIT_FAILURE,"Output file cannot be opened");
 }
+if ((picture = fopen (argv[3],"r")) == NULL) {
+	errx(EXIT_FAILURE,"Pic file cannot be opened");
+}
 if (fread(&WAVheader, sizeof(WAVheader), 1, audio_file)<1) {
 	errx(EXIT_FAILURE,"Audio could not be read");
 }
+if (fread(&BMPheader, sizeof(BMPheader), 1, picture)<1) {
+	errx(EXIT_FAILURE,"Pic could not be read");
+}
 
 info_audio();
-
 audioData = (char*) malloc(WAVheader.subchunk2Size);
 fread(audioData, WAVheader.subchunk2Size, 1, audio_file);
-
 fwrite(&WAVheader, sizeof(WAVheader), 1, output_file);
 fwrite(audioData, WAVheader.subchunk2Size, 1, output_file);
-
 free(audioData);
-fclose(audio_file); 
+fclose(audio_file);
+printf("audio written\n");
+
+info_pic();
+picData = (char*) malloc(BMPheader.dataSize);
+fread(picData, BMPheader.dataSize, 1, picture);
+fwrite(&BMPheader, sizeof(BMPheader), 1, output_file);
+fwrite(picData, BMPheader.dataSize, 1, output_file);
+free(picData);
+fclose(picture);
+printf("picture written\n");
+
 fclose(output_file);
 printf("finished. press any key to exit");
 getchar();
